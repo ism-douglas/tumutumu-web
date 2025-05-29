02093313f4803858047531b424838ee68efb80d2
Douglas Sabwa Indumwa <dglsdeveloper@gmail.com>
2025-05-29 07:57:09 +0300

    Feat: Add relevant footer content

--- website/index.php
+++ website/index.php
@@ -608,64 +608,64 @@
     <div class="container footer-top">
       <div class="row gy-4">
         <div class="col-lg-4 col-md-6 footer-about">
-          <a href="index.html" class="logo d-flex align-items-center">
-            <span class="sitename">NiceSchool</span>
+          <a href="./" class="logo d-flex align-items-center">
+            <span class="sitename">Tumutumu Girls</span>
           </a>
           <div class="footer-contact pt-3">
-            <p>A108 Adam Street</p>
-            <p>New York, NY 535022</p>
-            <p class="mt-3"><strong>Phone:</strong> <span>+1 5589 55488 55</span></p>
-            <p><strong>Email:</strong> <span>info@example.com</span></p>
+            <p>P.O. BOX 197-10101</p>
+            <p>Karatina, Nyeri County</p>
+            <p class="mt-3"><strong>Phone:</strong> <span>+254 799 313 672</span></p>
+            <p><strong>Email:</strong> <span><a href="mailto:admin@tumutumugirls.sc.ke">admin@tumutumugirls.sc.ke</a></span></p>
           </div>
           <div class="social-links d-flex mt-4">
-            <a href=""><i class="bi bi-twitter-x"></i></a>
-            <a href=""><i class="bi bi-facebook"></i></a>
-            <a href=""><i class="bi bi-instagram"></i></a>
-            <a href=""><i class="bi bi-linkedin"></i></a>
+            <a href="#0"><i class="bi bi-twitter-x"></i></a>
+            <a href="#0"><i class="bi bi-facebook"></i></a>
+            <a href="#0"><i class="bi bi-instagram"></i></a>
+            <a href="#0"><i class="bi bi-linkedin"></i></a>
           </div>
         </div>
 
         <div class="col-lg-2 col-md-3 footer-links">
           <h4>Useful Links</h4>
           <ul>
-            <li><a href="#">Home</a></li>
+            <li><a href="#">Homepage</a></li>
             <li><a href="#">About us</a></li>
-            <li><a href="#">Services</a></li>
-            <li><a href="#">Terms of service</a></li>
-            <li><a href="#">Privacy policy</a></li>
+            <li><a href="#">Our Programs</a></li>
+            <li><a href="#">News and Blog</a></li>
+            <li><a href="#">Events Calendar </a></li>
           </ul>
         </div>
 
         <div class="col-lg-2 col-md-3 footer-links">
-          <h4>Our Services</h4>
+          <h4>Academics</h4>
           <ul>
-            <li><a href="#">Web Design</a></li>
-            <li><a href="#">Web Development</a></li>
-            <li><a href="#">Product Management</a></li>
-            <li><a href="#">Marketing</a></li>
-            <li><a href="#">Graphic Design</a></li>
+            <li><a href="#">Students Portal</a></li>
+            <li><a href="#">Teachers Portal</a></li>
+            <li><a href="#">KSCE Results</a></li>
+            <li><a href="#">School Library</a></li>
+            <li><a href="#">Computer Lab</a></li>
           </ul>
         </div>
 
         <div class="col-lg-2 col-md-3 footer-links">
-          <h4>Hic solutasetp</h4>
+          <h4>Stakeholders</h4>
           <ul>
-            <li><a href="#">Molestiae accusamus iure</a></li>
-            <li><a href="#">Excepturi dignissimos</a></li>
-            <li><a href="#">Suscipit distinctio</a></li>
-            <li><a href="#">Dilecta</a></li>
-            <li><a href="#">Sit quas consectetur</a></li>
+            <li><a href="#">Admissions</a></li>
+            <li><a href="#">Our History</a></li>
+            <li><a href="#">Tender Notices</a></li>
+            <li><a href="#">Privacy Policy</a></li>
+            <li><a href="#">Report Abuse</a></li>
           </ul>
         </div>
 
         <div class="col-lg-2 col-md-3 footer-links">
-          <h4>Nobis illum</h4>
+          <h4>Others Links</h4>
           <ul>
-            <li><a href="#">Ipsam</a></li>
-            <li><a href="#">Laudantium dolorum</a></li>
-            <li><a href="#">Dinera</a></li>
-            <li><a href="#">Trodelas</a></li>
-            <li><a href="#">Flexo</a></li>
+            <li><a href="#">KNEC</a></li>
+            <li><a href="#">TSC</a></li>
+            <li><a href="#">STEM</a></li>
+            <li><a href="#">ASPNET</a></li>
+            <li><a href="#">YSK</a></li>
           </ul>
         </div>
 
@@ -673,13 +673,13 @@
     </div>
 
     <div class="container copyright text-center mt-4">
-      <p>© <span>Copyright</span> <strong class="px-1 sitename">MyWebsite</strong> <span>All Rights Reserved</span></p>
+      <p>© 2025<span></span> <strong class="px-1 sitename">Tumutumu Girls High School</strong> <span></span></p>
       <div class="credits">
         <!-- All the links in the footer should remain intact. -->
         <!-- You can delete the links only if you've purchased the pro version. -->
         <!-- Licensing information: https://bootstrapmade.com/license/ -->
         <!-- Purchase the pro version with working PHP/AJAX contact form: [buy-url] -->
-        Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
+        <!-- Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a> -->
       </div>
     </div>
 
