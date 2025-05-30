2e8ac61ed7308b336ff4b51d76b14d601cc0d6f9
Douglas Sabwa Indumwa <dglsdeveloper@gmail.com>
2025-05-30 08:09:00 +0300

    Feat: Add dynamic footer

---
+++ website/footer.php
@@ -0,0 +1,81 @@
+  <footer id="footer" class="footer position-relative dark-background">
+
+    <div class="container footer-top">
+      <div class="row gy-4">
+        <div class="col-lg-4 col-md-6 footer-about">
+          <a href="./" class="logo d-flex align-items-center">
+            <span class="sitename">Tumutumu Girls</span>
+          </a>
+          <div class="footer-contact pt-3">
+            <p>P.O. BOX 197-10101</p>
+            <p>Karatina, Nyeri County</p>
+            <p class="mt-3"><strong>Phone:</strong> <span>+254 799 313 672</span></p>
+            <p><strong>Email:</strong> <span><a href="mailto:admin@tumutumugirls.sc.ke">admin@tumutumugirls.sc.ke</a></span></p>
+          </div>
+          <div class="social-links d-flex mt-4">
+            <a href="#0"><i class="bi bi-twitter-x"></i></a>
+            <a href="#0"><i class="bi bi-facebook"></i></a>
+            <a href="#0"><i class="bi bi-instagram"></i></a>
+            <a href="#0"><i class="bi bi-linkedin"></i></a>
+          </div>
+        </div>
+
+        <div class="col-lg-2 col-md-3 footer-links">
+          <h4>Useful Links</h4>
+          <ul>
+            <li><a href="./">Homepage</a></li>
+            <li><a href="about.php">About us</a></li>
+            <li><a href="academics.php">Our Programs</a></li>
+            <li><a href="events.php">News and Blog</a></li>
+            <li><a href="events.php">Events Calendar </a></li>
+          </ul>
+        </div>
+
+        <div class="col-lg-2 col-md-3 footer-links">
+          <h4>Academics</h4>
+          <ul>
+            <li><a href="https://portal.tumutumugirls.sc.ke/">Students Portal</a></li>
+            <li><a href="https://teachers.tumutumugirls.sc.ke/">Teachers Portal</a></li>
+            <li><a href="https://results.knec.ac.ke">KSCE Results</a></li>
+            <li><a href="academics.php">School Library</a></li>
+            <li><a href="academics.php">Computer Lab</a></li>
+          </ul>
+        </div>
+
+        <div class="col-lg-2 col-md-3 footer-links">
+          <h4>Stakeholders</h4>
+          <ul>
+            <li><a href="contact.php">Admissions</a></li>
+            <li><a href="about.php">Our History</a></li>
+            <li><a href="contact.php">Tender Notices</a></li>
+            <li><a href="contact.php">Privacy Policy</a></li>
+            <li><a href="contact.php">Report Abuse</a></li>
+          </ul>
+        </div>
+
+        <div class="col-lg-2 col-md-3 footer-links">
+          <h4>Others Links</h4>
+          <ul>
+            <li><a href="https://www.knec.ac.ke/">KNEC</a></li>
+            <li><a href="https://www.tsc.go.ke/">TSC</a></li>
+            <li><a href="https://vision2030.go.ke/project/development-of-the-science-technology-engineering-and-mathematics-sector/">STEM</a></li>
+            <li><a href="https://www.unesco.org/en/aspnet">ASPNET</a></li>
+            <li><a href="https://ysk.co.ke/">YSK</a></li>
+          </ul>
+        </div>
+
+      </div>
+    </div>
+
+    <div class="container copyright text-center mt-4">
+      <p>© 2025<span></span> <strong class="px-1 sitename">Tumutumu Girls High School</strong> <span></span></p>
+      <div class="credits">
+        <!-- All the links in the footer should remain intact. -->
+        <!-- You can delete the links only if you've purchased the pro version. -->
+        <!-- Licensing information: https://bootstrapmade.com/license/ -->
+        <!-- Purchase the pro version with working PHP/AJAX contact form: [buy-url] -->
+        <!-- Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a> -->
+      </div>
+    </div>
+
+  </footer>
