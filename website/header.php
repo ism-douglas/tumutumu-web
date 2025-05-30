cd9d3ec8dddbda5d6accd0b2bc1074015fa832e0
Douglas Sabwa Indumwa <dglsdeveloper@gmail.com>
2025-05-30 08:19:31 +0300

    Feat: Add dynamic header file

---
+++ website/header.php
@@ -0,0 +1,46 @@
+  <header id="header" class="header d-flex align-items-center fixed-top">
+    <div class="container-fluid container-xl position-relative d-flex align-items-center justify-content-between">
+
+      <a href="./" class="logo d-flex align-items-center">
+        <!-- Uncomment the line below if you also wish to use an image logo -->
+        <img src="assets/img/tumutumu.webp" alt="Tumutumu Girls Logo">
+        <!-- <i class="bi bi-buildings"></i> -->
+        <h5 class="sitename">Tumutumu Girls</h5>
+      </a>
+
+      <nav id="navmenu" class="navmenu">
+        <ul>
+          <li><a href="./" class="active">Home</a></li>
+          <li class="dropdown"><a href="about.php"><span>About</span> <i class="bi bi-chevron-down toggle-dropdown"></i></a>
+            <ul>
+              <li><a href="about.php">Our Profile</a></li>
+              <li><a href="academics.php">Academics</a></li>
+              <li><a href="principal.php">Chief Principal</a></li>
+              <li><a href="anthem.php">School Anthem</a></li>
+            </ul>
+          </li>
+          <li class="dropdown"><a href="about.php"><span>Media</span> <i class="bi bi-chevron-down toggle-dropdown"></i></a>
+            <ul>
+              <li><a href="events.php">Events</a></li>
+              <li><a href="news.php">News</a></li>
+              <li><a href="#0">Gallery</a></li>
+              <li><a href="#0">Videos</a></li>
+            </ul>
+          </li>
+
+          <li><a href="student.php">Students Life</a></li>         
+          <li><a href="alumni.php">Alumni</a></li>
+          <li class="dropdown"><a href="#"><span>eLearning</span> <i class="bi bi-chevron-down toggle-dropdown"></i></a>
+            <ul>
+              <li><a href="https://portal.tumutumugirls.sc.ke/">Students Portal</a></li>
+              <li><a href="https://teachers.tumutumugirls.sc.ke/">Teachers Portal</a></li>
+              
+            </ul>
+          </li>        
+          <li><a href="contact.php">Contact</a></li>
+        </ul>
+        <i class="mobile-nav-toggle d-xl-none bi bi-list"></i>
+      </nav>
+
+    </div>
+  </header>
